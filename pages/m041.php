d83ec870522a95bbebc72304dbd1531c39b9c17d
j3ongjisu <wjdwltn911@naver.com>
2023-07-03 14:03:27 +0900

    230703

---
+++ pages/m041.php
@@ -0,0 +1,24 @@
+<? include '../head.php' ?>
+<?
+$num = 4;
+$cate_title = '제품';
+$title = 'ddd4';
+?>
+
+<? include '../sub_title.php' ?>
+<article class="article">
+            <div class="inner">
+                <h2><?=$title?></h2>
+                <div class="poem">
+                    <p>
+                        이것은 마음에만 보이는 아름다운 시 01
+                    </p>
+                    <figure>
+                        <img src="./images/lesedilarona01.jpg" alt="">
+                    </figure>
+                </div>
+            </div>
+        </article>
+
+        <? include '../tail.php' ?>
+
---
+++ tail.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+            <div class="inner">
+                &copy; jeong
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
